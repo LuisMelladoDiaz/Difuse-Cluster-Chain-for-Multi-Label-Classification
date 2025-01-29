ce9afafed7231fc6bc053f1573e5a473abd8355e
LuisMelladoDiaz <melladodiazluis@gmail.com>
2025-01-29 19:03:07 +0100

    refactor: examples of use

--- src/main.py
+++ src/main.py
@@ -2,30 +2,38 @@ from LCC_MLC import *
 from CC import train_CC, train_ECC
 from preprocess import load_csv_and_train_test_split
 
-## Clasifier Chain ##################################################################################################################3
 
-file = 'datasets/binario/diabetes.csv'
-X_train, X_test, y_train, y_test = load_csv_and_train_test_split(file)
+## Clasifier Chain ###########################################################################################################################################################################################
 
-Y_pred_CC = train_CC(X_train, y_train, X_test)
-Y_pred_ECC_chains, Y_pred_ECC_ensemble = train_ECC(X_train, y_train, X_test)
+def example_CC():
+    file = 'datasets/binario/diabetes.csv'
+    X_train, X_test, y_train, y_test = load_csv_and_train_test_split(file)
 
-print("Predicciones con Classifier Chain (CC):")
-print(Y_pred_CC)
+    Y_pred_CC = train_CC(X_train, y_train, X_test)
 
-print("Predicciones con Ensemble of Classifier Chains (ECC):")
-print("Predicciones por cada cadena:")
-print(Y_pred_ECC_chains)
+    print("Predicciones con Classifier Chain (CC):")
+    print(Y_pred_CC)
 
-print("Predicciones del ensemble:")
-print(Y_pred_ECC_ensemble)
+def example_ECC():
+    file = 'datasets/binario/diabetes.csv'
+    X_train, X_test, y_train, y_test = load_csv_and_train_test_split(file)
 
-## LLC-MLC ##################################################################################################################3
+    Y_pred_ECC_chains, Y_pred_ECC_ensemble = train_ECC(X_train, y_train, X_test)
 
-data_folder = 'datasets/multi_etiqueta/Birds/birds'
-train_file = f'{data_folder}-train.arff'
-test_file = f'{data_folder}-test.arff'
-q_labels = 19
-n_clusters = 6
+    print("Predicciones del ensemble ECC:")
+    print(Y_pred_ECC_ensemble)
 
-#LLC_MLC(train_file, q_labels, num_clusters=n_clusters)
+## LCC-MLC ###########################################################################################################################################################################################
+
+def example_LLC_MLC():
+    data_folder = 'datasets/multi_etiqueta/Birds/birds'
+    train_file = f'{data_folder}-train.arff'
+    test_file = f'{data_folder}-test.arff'
+    q_labels = 19
+
+    LCC_MLC(train_file, q_labels)
+
+## Examples of use ###########################################################################################################################################################################################
+example_CC()
+example_ECC()
+example_LLC_MLC()
