2b2e9d57236ff0f0e22210f3a1e81d60370507a4
LuisMelladoDiaz <melladodiazluis@gmail.com>
2025-01-30 19:44:14 +0100

    feat(LDCC): in progress

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from models.LCC_MLC import *
+from models.LCC_MLC import LCC_MLC
 from models.CC import train_CC, train_ECC
+from models.LDCC_MLC import LCC_MLC_Fuzzy
 from utils.preprocessing import load_csv_and_train_test_split
 
 
@@ -25,7 +26,7 @@ def example_ECC():
 
 ## LCC-MLC ###########################################################################################################################################################################################
 
-def example_LLC_MLC():
+def example_LCC_MLC():
     data_folder = 'datasets/multi_etiqueta/Birds/birds'
     train_file = f'{data_folder}-train.arff'
     test_file = f'{data_folder}-test.arff'
@@ -33,7 +34,18 @@ def example_LLC_MLC():
 
     LCC_MLC(train_file, q_labels)
 
+## LDCC-MLC ###########################################################################################################################################################################################
+
+def example_LDCC_MLC():
+    data_folder = 'datasets/multi_etiqueta/Birds/birds'
+    train_file = f'{data_folder}-train.arff'
+    test_file = f'{data_folder}-test.arff'
+    q_labels = 19
+
+    LCC_MLC_Fuzzy(train_file, q_labels)
+
 ## Examples of use ###########################################################################################################################################################################################
-example_CC()
-example_ECC()
-example_LLC_MLC()
+#example_CC()
+#example_ECC()
+example_LCC_MLC()
+#example_LDCC_MLC()
